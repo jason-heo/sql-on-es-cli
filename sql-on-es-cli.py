f3780788860534b94cdd5f891f79314454c6d772
JS Heo <mysqlguru@github.com>
2015-11-07 23:44:24 +0900

    SQL 수행 후 결과를 Json으로 가져오기

--- sql-on-es-cli.py
+++ sql-on-es-cli.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import sys
 import readline
 import os
+import json
+
+if len(sys.argv) == 1:
+    sys.stderr.write("Usage: {0} <endpoint>\n\n".format(sys.argv[0]))
+    sys.stderr.write("Ex) {0} http://localhost:9200\n".format(sys.argv[0]))
+    sys.exit(1)
+
+endpoint = sys.argv[1]
 
 hist_file = os.path.join(os.path.expanduser("~"), ".sql-on-es.hist")
 
@@ -14,13 +23,46 @@ except (IOError):
 import atexit
 atexit.register(readline.write_history_file, hist_file)
 
+import httplib
+import urlparse
+import urllib
+
 # readline.parse_and_bind('tab: complete')
 # readline.parse_and_bind('set editing-mode emacs')
 
+class HttpClient:
+    def __init__(self, endpoint):
+        o = urlparse.urlparse(endpoint)
+
+        self.protocol = o.scheme
+        self.hostname = o.hostname
+        self.port = o.port
+    
+    def get(self, sql):
+
+        conn = httplib.HTTPConnection(self.hostname, self.port)
+        conn.request("GET", "/_sql?sql=" + urllib.quote(sql))
+
+        response = conn.getresponse()
+
+        return json.loads(response.read())
+
+class SQLExecutor:
+    def __init__(self, endpoint):
+        self.http_client = HttpClient(endpoint)
+    
+    def run(self, sql):
+        json_obj = self.http_client.get(sql)
+
+        print json.dumps(json_obj, indent = 2)
+
+executor = SQLExecutor(endpoint)
+
 while True:
     try:
-        line = raw_input('SQL> ')
-        print 'Entered: "%s"' % line
+        sql = raw_input('SQL> ')
+        executor.run(sql)
     except (EOFError):
         break
 
+print "\nexit..."
